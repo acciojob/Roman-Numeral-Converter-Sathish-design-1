0b5b6c8a6cb5ca5d4b9462e1fe28a79a60042022
SATHISH B <sathishb2310@gmail.com>
2025-09-29 15:33:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -10,7 +10,17 @@ function convertToRoman(num) {
     };
 
   //your code here
+	let roman = '';
+	const valus = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4,1];
+	const symbols = ['M','CM','D','CD','C','XC','L','XL','X','IX','V','IV','I'];
 
+	for(let i = 0; i < valus.length; i++){
+		if(num > valus[i]){
+			roman += symbols[i];
+			num -= valus[i];
+		}
+	}
+	return;
 }
 // You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
 
